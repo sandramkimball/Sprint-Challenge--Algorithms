cd372ee6234f041b74400e70ae028323b7b1e5a2
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-02-21 11:46:25 -0800

    count_th fully works now

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,21 +6,16 @@ Your function must utilize recursion. No loops.
 def count_th(word):
     count = 0
     i = 0
-    
-    if i == len(word):
-        return count
-        
-    if word[i] + word[i+1] == 'th' and i < len(word)-1:
-        print(f'if: {word[i]}{word[i+1]}')
-        i += 1
-        count += 1
 
-    else:
-        i += 1
-        print(f'else: {word[i]}{word[i+1]}')
+    if len(word) >= 2 and i < len(word)-1:
+        if word[:2] == 'th':
+            count += 1
+            i += 1
+            return 1 + count_th(word[2:])
+        else: 
+            return 0 + count_th(word[1:])
 
-    
-    count_th(word)
+    # count += count_th(word)
     return count
 
 
